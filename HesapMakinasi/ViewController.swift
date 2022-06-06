ed15c2ff4506c44ae4b575edb430c169ecb5c7e1
turan çabuk <turan6677@gmail.com>
2022-06-06 19:46:00 +0300

    Calculator Project

--- HesapMakinasi/ViewController.swift
+++ HesapMakinasi/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var FirstText: UITextField!
+    @IBOutlet weak var SecondText: UITextField!
+    
+    @IBOutlet weak var ResultLabel: UILabel!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    
     }
-
+    @IBAction func Topla(_ sender: Any) {
+        
+        if let firstnumber = Double(FirstText.text!) {
+            if let secondnumber = Double(SecondText.text!){
+                let result = firstnumber + secondnumber
+                ResultLabel.text = "Result: \(result)"
+            }
+        }
+        
+    }
+    @IBAction func Cikar(_ sender: Any) {
+        
+        if let firstnumber = Double(FirstText.text!){
+            if let secondnumber = Double(SecondText.text!){
+                let result = firstnumber-secondnumber
+                ResultLabel.text = "Result: \(result)"
+            }
+        }
+    }
+    @IBAction func Carp(_ sender: Any) {
+        
+        if let firstnumber = Double(FirstText.text!){
+            if let secondnumber = Double(SecondText.text!){
+                let result = firstnumber*secondnumber
+                ResultLabel.text = "Result: \(result)"
+            }
+        }
+    }
+    @IBAction func Bol(_ sender: Any) {
+        
+        if let firstnumber = Double(FirstText.text!){
+            if let secondnumber = Double(SecondText.text!){
+                let result = firstnumber/secondnumber
+                ResultLabel.text = "Result: \(result)"
+            }
+        }
+        
+    }
+    
 
 }
 
